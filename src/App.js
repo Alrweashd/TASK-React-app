6c313dfbe46f06f764755433b59a7d3d4c02a5c7
alrweashd <trkyabler@gmail.com>
2023-05-28 18:03:33 +0300

    Solved

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./assets/css/start.css";
+import image from "./assets/images/desktop_thumbnail_5c212d5c-ea46-44cf-9350-78d14e98ae42.jpeg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <header> Pizza Hut</header>
+        <h2>Pizza hut </h2>
+        <p>THe food is here</p>
+        <img src={image} className="img1" alt="logo" />
+      </div>
+    </>
   );
 }
 
